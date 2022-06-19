fb040cf443b89b3ee6ec0e6ec6211fb7925b963e
Nattatorn Yucharoen <nattatorn.dev@gmail.com>
2022-06-19 23:21:10 +0700

    Init

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import dayjs from 'dayjs';
-
 import utc from 'dayjs/plugin/utc';
 import timezone from 'dayjs/plugin/timezone';
 
@@ -9,14 +8,14 @@ dayjs.extend(timezone);
 const date = dayjs();
 console.log('-----------Local------------');
 console.log('Timezone \n', dayjs.tz.guess());
-console.log('ISO Date \n', date.local().toISOString());
-const localStart = date.local().startOf('day').toISOString();
-const localEnd = date.local().endOf('day').toISOString();
+console.log('ISO Date \n', date.local().format());
+const localStart = date.local().startOf('day').format();
+const localEnd = date.local().endOf('day').format();
 console.log('End of day \n', localStart, localEnd);
 
 const tz = 'Asia/Bangkok';
 console.log('--------Asia/Bangkok--------');
-console.log('ISO Date \n', dayjs.tz(date, tz).utc().toISOString());
-const start = dayjs.tz(date, tz).startOf('day').utc().toISOString();
-const end = dayjs.tz(date, tz).endOf('day').utc().toISOString();
+console.log('ISO Date \n', dayjs.tz(date, tz).utc().format());
+const start = dayjs.tz(date, tz).startOf('day').utc().format();
+const end = dayjs.tz(date, tz).endOf('day').utc().format();
 console.log('End of day \n', start, end);
