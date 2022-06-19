dd81141797967a00bd090ccf39c2ef6547f754ca
Nattatorn Yucharoen <nattatorn.dev@gmail.com>
2022-06-19 23:37:42 +0700

    Init

--- src/index.ts
+++ src/index.ts
@@ -6,7 +6,7 @@ dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const date = dayjs();
-console.log('-----------Local------------');
+console.log('-------------Local--------------');
 console.log('Timezone \n', dayjs.tz.guess());
 console.log('ISO Date \n', date.local().format());
 const localStart = date.local().startOf('day').format();
@@ -14,7 +14,7 @@ const localEnd = date.local().endOf('day').format();
 console.log('End of day \n', localStart, localEnd);
 
 const tz = 'Asia/Bangkok';
-console.log('--------Asia/Bangkok--------');
+console.log('--------UTC-Asia/Bangkok--------');
 console.log('ISO Date \n', dayjs.tz(date, tz).utc().format());
 const start = dayjs.tz(date, tz).startOf('day').utc().format();
 const end = dayjs.tz(date, tz).endOf('day').utc().format();
